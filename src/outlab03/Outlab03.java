af7019db03de6a2a5d78b5d66cc4f408e89ef76d
mathmaster12 <36994858+mathmaster12@users.noreply.github.com>
2018-04-10 20:04:37 -0600

    Update Outlab03.java

--- src/outlab03/Outlab03.java
+++ src/outlab03/Outlab03.java
@@ -9,40 +9,43 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-class Edge implements Comparable<Edge> {
-    char u;
-    char v;
-    int weight;
+class Edge implements Comparable<Edge> { //queue for edges 
+    int u; //start point
+    int v; //end point
+    int weight; //weight of edge
     public int compareTo(Edge compareEdge) {
         return this.weight-compareEdge.weight;
     }
 }
 
-class Cluster{
-    char parent;
+class Cluster{ 
+    int parent;
     int rank;
 }
 
 class Graph{
-	static int V;
-	int E; 
+	static int V; //number of vertices
+	int E; //number of edges 
 	static Edge edges[]; 
 	
     public Graph(int n, int m) {
-        V=n;
-        E=m;
+        V=n; //number of vertices 
+        E=m; //number of edges
         edges = new Edge[E]; 
         for(int x = 0; x < E; ++x){
         	edges[x] = new Edge(); 
         }
     }
+    //get value of V
 	public int V(){
 		return V;
 	}
+	//get value of E
 	public int E(){
 		return E;
 	}
-	static char find(Cluster clusters[], char x){
+	//get cluster
+	static int find(Cluster clusters[], int x){
 	    if (clusters[x].parent != x){
 	        clusters[x].parent = find(clusters, clusters[x].parent);
 	    }
@@ -50,9 +53,9 @@ class Graph{
 	}
 	
 	//Merge two clusters together
-	 static void Merge(Cluster cluster[], char x, char y){
-	        char u = find(cluster, x); //C(u)
-	        char v = find(cluster, y); //C(v)
+	 static void Merge(Cluster cluster[], int x, int y){
+	        int u = find(cluster, x); //C(u)
+	        int v = find(cluster, y); //C(v)
 	        
 	        if (cluster[u].rank < cluster[v].rank){ //if C(u) < C(v)
 	            cluster[u].parent = v;
@@ -67,37 +70,52 @@ class Graph{
 	    }
 }
 
-public class Program3 {
+public class outlab03 {
 	public static void main(String args[]){ 
-		 int V = 4;  // Number of vertices in graph
-	     int E = 5;  // Number of edges in graph
+		 int V = 6;  // Number of vertices in graph
+	     int E = 8;  // Number of edges in graph
 	     Graph graph = new Graph(V, E);
 	 
 	        // add edge 0-1
-	        graph.edges[0].u = 'A';
-	        graph.edges[0].v = 'B';
-	        graph.edges[0].weight = 10;
-	 
-	        // add edge 0-2
-	        graph.edges[1].u = 'A';
-	        graph.edges[1].v = 'C';
-	        graph.edges[1].weight = 6;
+	        graph.edges[0].u = 0;
+	        graph.edges[0].v = 1;
+	        graph.edges[0].weight = 5;
 	 
 	        // add edge 0-3
-	        graph.edges[2].u = 'A';
-	        graph.edges[2].v = 'D';
-	        graph.edges[2].weight = 5;
+	        graph.edges[1].u = 0;
+	        graph.edges[1].v = 3;
+	        graph.edges[1].weight = 2;
 	 
-	        // add edge 1-3
-	        graph.edges[3].u = 'B';
-	        graph.edges[3].v = 'D';
-	        graph.edges[3].weight = 15;
+	        // add edge 0-4
+	        graph.edges[2].u = 0;
+	        graph.edges[2].v = 4;
+	        graph.edges[2].weight = 7;
 	 
-	        // add edge 2-3
-	        graph.edges[4].u = 'C';
-	        graph.edges[4].v = 'D';
-	        graph.edges[4].weight = 4;
-		
+	        // add edge 1-2
+	        graph.edges[3].u = 1;
+	        graph.edges[3].v = 2;
+	        graph.edges[3].weight = 6;
+	 
+	        // add edge 2-4
+	        graph.edges[4].u = 2;
+	        graph.edges[4].v = 4;
+	        graph.edges[4].weight = 8;
+	        
+	        //add edge 2-5
+	        graph.edges[5].u = 2;
+	        graph.edges[5].v = 5;
+	        graph.edges[5].weight = 3;
+	        
+	        //add edge 3-4
+	        graph.edges[6].u = 3;
+	        graph.edges[6].v = 4;
+	        graph.edges[6].weight = 4;
+	        
+	        //add edge 4-5
+	        graph.edges[7].u = 4;
+	        graph.edges[7].v = 5;
+	        graph.edges[7].weight = 5;
+	        
 		KruskalMST(graph);
 	}
 	 //Kruskal’s Algorithm to find the minimum spanning tree for a weighted graph
@@ -116,17 +134,13 @@ public class Program3 {
 	            cluster[v].parent = v;
 	            cluster[v].rank = 0;
 	        }
-	        PriorityQueue<Integer> Q = new PriorityQueue<Integer>(); 
-	        for (int i = 0; i < G.V; ++i){
-	        	Q.add(G.edges[i].weight); 
-	        }
 	        int x = 0;
 	        int edge = 0; 
 	        while (edge < G.V - 1){
 	            Edge next_edge = new Edge();
 	            next_edge = G.edges[x++];
-	            char y = G.find(cluster, next_edge.u); //C(u)
-	            char z = G.find(cluster, next_edge.v); //C(v)
+	            int y = G.find(cluster, next_edge.u); //C(u)
+	            int z = G.find(cluster, next_edge.v); //C(v)
 	            if (y != z){ //if C(u) != C(v)
 	            	//add edge to Tree
 	                T[edge++] = next_edge;
